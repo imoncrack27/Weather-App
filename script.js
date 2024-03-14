2ec17f61a95d388648243b75f2ab484dd7c2bebe
imoncrack27 <mingodrexell321@gmail.com>
2024-03-14 13:20:10 +0800

    fixed the problem where input a city wont show, now it shows the weather if you input any city

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
-const container = document.querySelector(".container");
-const search = document.querySelector(".search-box button");
-const weatherBox = document.querySelector(".weather-box");
-const weatherDetails = document.querySelector(".weather-details");
+const container = document.querySelector(`.container`);
+const search = document.querySelector(`.search-box button`);
+const weatherBox = document.querySelector(`.weather-box`);
+const weatherDetails = document.querySelector(`.weather-details`);
 
-search.addEventListener("click", () => {
-  const city = document.querySelector(".search-box input").value;
+search.addEventListener(`click`, () => {
+  const API_KEY = `d6af9c861d995d8fc94f8f6cb006833f`;
+  const city = document.querySelector(`.search-box input`).value;
 
-  if (city == "") return;
+  if (city == ``) return;
 
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid={d6af9c861d995d8fc94f8f6cb006833f}"
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
   )
     .then((response) => response.json())
     .then((json) => {
-      const image = document.querySelector(".weather-box img");
-      const temperature = document.querySelector(".weather-box .temperature");
-      const description = document.querySelector(".weather-box description");
+      const image = document.querySelector(`.weather-box img`);
+      const temperature = document.querySelector(`.weather-box .temperature`);
+      const description = document.querySelector(`.weather-box description`);
       const humidity = document.querySelector(
-        ".weather-details .humidity span"
+        `.weather-details .humidity span`
       );
-      const wind = document.querySelector(".weather-details .wind span");
+      const wind = document.querySelector(`.weather-details .wind span`);
 
       switch (json.weather[0].main) {
-        case "Clear":
-          image.src = "images/clear.png";
+        case `Clear`:
+          image.src = `images/clear.png`;
           break;
 
-        case "Rain":
-          image.src = "images/rain.png";
+        case `Rain`:
+          image.src = `images/rain.png`;
           break;
 
-        case "Snow":
-          image.src = "images/snow.png";
+        case `Snow`:
+          image.src = `images/snow.png`;
           break;
 
-        case "Clouds":
-          image.src = "images/cloud.png";
+        case `Clouds`:
+          image.src = `images/cloud.png`;
           break;
 
-        case "Haze  ":
-          image.src = "images/mist.png";
+        case `Haze  `:
+          image.src = `images/mist.png`;
           break;
 
         default:
-          image.src = "images/cloud.png";
+          image.src = `images/cloud.png`;
       }
 
-      temperature.innerHTML = "${parseInt(json.main.temp)}<span>°C</span>";
-      description.innerHTML = "${json.weather[0].description}";
-      humidity.innerHTML = "${json.main.humidity}%";
-      wind.innerHTML = "${parseInt(json.wind.speed)}Km/h";
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
     });
 });
