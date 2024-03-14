9997b11af60ba095b064c9747b4c0a4881c91395
imoncrack27 <mingodrexell321@gmail.com>
2024-03-14 15:58:03 +0800

    Everything is running now after debugging for 4 hours

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector(`.container`);
 const search = document.querySelector(`.search-box button`);
 const weatherBox = document.querySelector(`.weather-box`);
 const weatherDetails = document.querySelector(`.weather-details`);
+const error404 = document.querySelector(`.not-found`);
 
 search.addEventListener(`click`, () => {
   const API_KEY = `d6af9c861d995d8fc94f8f6cb006833f`;
@@ -10,13 +11,27 @@ search.addEventListener(`click`, () => {
   if (city == ``) return;
 
   fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
   )
     .then((response) => response.json())
     .then((json) => {
+      if (json.cod == `404`) {
+        container.style.height = `400px`;
+        weatherBox.classList.remove(`active`);
+        weatherDetails.classList.remove(`active`);
+        error404.classList.add(`active`);
+
+        return;
+      }
+
+      container.style.height = `555px`;
+      weatherBox.classList.add(`active`);
+      weatherDetails.classList.add(`active`);
+      error404.classList.remove(`active`);
+
       const image = document.querySelector(`.weather-box img`);
       const temperature = document.querySelector(`.weather-box .temperature`);
-      const description = document.querySelector(`.weather-box description`);
+      const description = document.querySelector(`.weather-box .description`);
       const humidity = document.querySelector(
         `.weather-details .humidity span`
       );
@@ -39,7 +54,11 @@ search.addEventListener(`click`, () => {
           image.src = `images/cloud.png`;
           break;
 
-        case `Haze  `:
+        case `Mist`:
+          image.src = `images/mist.png`;
+          break;
+
+        case `Haze`:
           image.src = `images/mist.png`;
           break;
 
