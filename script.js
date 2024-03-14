a7920f4676a2ccd08021dd0aea933ec24a1f9f57
imoncrack27 <mingodrexell321@gmail.com>
2024-03-14 16:23:22 +0800

    added infoweather css and js

--- script.js
+++ script.js
@@ -40,44 +40,50 @@ search.addEventListener(`click`, () => {
         cityHide.textContent = city;
 
         container.style.height = `555px`;
+        container.classList.add(`active`);
         weatherBox.classList.add(`active`);
         weatherDetails.classList.add(`active`);
         error404.classList.remove(`active`);
+
+        setTimeout(() => {
+          container.classList.remove(`active`);
+        }, 2500);
 
         switch (json.weather[0].main) {
           case `Clear`:
             image.src = `images/clear.png`;
             break;
-  
+
           case `Rain`:
             image.src = `images/rain.png`;
             break;
-  
+
           case `Snow`:
             image.src = `images/snow.png`;
             break;
-  
+
           case `Clouds`:
             image.src = `images/cloud.png`;
             break;
-  
+
           case `Mist`:
             image.src = `images/mist.png`;
             break;
-  
+
           case `Haze`:
             image.src = `images/mist.png`;
             break;
-  
+
           default:
             image.src = `images/cloud.png`;
         }
-  
+
         temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
         description.innerHTML = `${json.weather[0].description}`;
         humidity.innerHTML = `${json.main.humidity}%`;
         wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+        const infoWeather
       }
-      
     });
 });
